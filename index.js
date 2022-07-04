77c8777c2ef7d360f85c1559305a93069f914ffd
Monika Gustilo <monikagustilo@gmail.com>
2022-07-03 22:45:47 -0700

    commit tree fdbc6b022ed22e2965aff2a996ed3cc83346754d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+  return arr.slice(0, 2);
+};
+
+// console.log(returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(-2);
+};
+
+// console.log(returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (integer) {
+  return function (fare) {
+    return fare * integer;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+// function selectDifferentDrivers(driversArray, cb) {
+//   if ((cb = returnFirstTwoDrivers)) {
+//     return returnFirstTwoDrivers(driversArray);
+//   } else if ((cb = returnLastTwoDrivers)) {
+//     return returnLastTwoDrivers(driversArray);
+//   }
+// }
+
+function selectDifferentDrivers(driversArray, returnFirstTwoDrivers) {
+  return returnFirstTwoDrivers(driversArray);
+}
